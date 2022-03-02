7e273bb8ba103348c9a7f0f10907bf11a0f14d36
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2022-03-02 11:12:23 -0800

    Finished template

--- RationalSet/RationalSet.cpp
+++ RationalSet/RationalSet.cpp
@@ -15,12 +15,6 @@ RationalSet::RationalSet (const RationalSet &rcRationalSet) {
 bool RationalSet::isIn (const Rational &rcRational) const {
 	bool bInSet = false;
 
-	//for (unsigned int i = 0; i < mcRationals.size () && !bInSet; ++i) {
-	//	if (mcRationals[i] == rcRational) {
-	//		bInSet = true;
-	//	}
-	//}
-
 	return bInSet;
 }
 
